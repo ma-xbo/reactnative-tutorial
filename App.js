ee92742625c57e2049d3cc9163b17d2c40e18659
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-02 08:55:25 +0100

    Box added

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <View style={styles.box1} />
       <Text>Open up App.js to start working on your app!</Text>
+      <Button title='Klick' />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +16,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  box1: {
+    backgroundColor: 'cyan',
+    width: '100%',
+    height: '20%'
+  }
 });
