c9853cb25456419d7635363c687c80df77400b65
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2020-12-02 11:01:43 +0100

    Update App.js
    props an eigene Komponente angefügt

--- App.js
+++ App.js
@@ -4,17 +4,21 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Counter />
+      <Counter startValue='0' />
+      <Counter startValue='10' />
+      <Counter startValue='100' />
     </View>
   );
 }
 
-// Eigene Komponente namens Counter
-function Counter() {
+// Eigene Komponente namens Counter, bekommt zur Laufzeit die props übergeben 
+function Counter(props) {
+  const startValue = props.startValue; // Zugriff auf die props
+
   // Liefert JSX Code zurück -> Kann direkt aufgerufen werden
   return (
     <View>
-      <Text style={styles.counter}>{1}</Text>
+      <Text style={styles.counter}>{startValue}</Text>
       <Button title='Klick' onPress={() => alert('clicked')} />
     </View>
   );
